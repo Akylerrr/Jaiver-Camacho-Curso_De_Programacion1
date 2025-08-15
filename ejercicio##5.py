22283e6f74f82621c50e0f25fbbe80a25bc50fb5
Akylerrr <jaivercam12@gmail.com>
2025-08-15 09:08:55 -0400

    tarea#5

---
+++ ejercicio##5.py
@@ -0,0 +1,6 @@
+base = float(input("ingresa la base del rectangulo: "))
+altura = float(input("ingrese la altura del rectangulo: "))
+area= base*altura
+perimetro = 2 *(base+altura)
+print("area del rectangulo:", area)
+print("perimetro del rectangulo:", perimetro)
